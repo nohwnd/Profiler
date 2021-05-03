2e7df5291e37bb9ad167078e08ae5bae19989fef
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2021-05-03 22:46:42 +0000

    added line and commandhit classes

---
+++ csharp/Profiler/CommandHit.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace Profiler
+{
+    /// <summary>
+    /// Represents a command hit on a line
+    /// </summary>
+    public class CommandHit
+    {
+        /// <summary>
+        /// Line number of the command in the file or scriptblock.
+        /// </summary>
+        public uint Line { get; set; }
+
+        /// <summary>
+        /// Column of the command in the file or scriptblock.
+        /// </summary>
+        public uint Column { get; set; }
+
+        /// <summary>
+        /// Number of hits on command.
+        /// </summary>
+        public uint HitCount { get; set; } = 1;
+
+        /// <summary>
+        /// Time used exclusively by this command
+        /// </summary>
+        public TimeSpan SelfDuration { get; set; }
+
+        /// <summary>
+        /// Command text.
+        /// </summary>
+        public string Text { get; set; }
+    }
+}
