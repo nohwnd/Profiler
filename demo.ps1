169419e37dfd4f9f913d878a27afb60a2c20b7a5
Jakub Jareš <me@jakubjares.com>
2022-06-10 21:54:13 +0200

    Fix function summary (#58)

--- demo.ps1
+++ demo.ps1
@@ -2,7 +2,8 @@
 
 Import-Module $PSScriptRoot/Profiler/Profiler.psd1 -Force
 
-$trace =  Trace-Script -ScriptBlock { & "$PSScriptRoot/demo-scripts/Get-Icons.ps1" } -ExportPath emojis.speedscope.json
+$trace =  Trace-Script -ScriptBlock {
+    & "$PSScriptRoot/demo-scripts/Get-Icons.ps1" } -ExportPath emojis.speedscope.json
 
 # Shows the top 50 lines that take the most percent of the run by themselves
 $trace.Top50SelfDuration | Format-Table
