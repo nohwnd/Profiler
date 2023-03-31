1a41d62f34fe4787262717ef10b045358c1831b3
Jakub Jareš <me@jakubjares.com>
2023-03-31 17:11:42 +0200

    Fix signature of external tracer adapter (#60)
    Fix signature of external tracer adapter to pass on functionName and moduleName.

--- csharp/Profiler/ExternalTracerAdapter.cs
+++ csharp/Profiler/ExternalTracerAdapter.cs
@@ -13,13 +13,13 @@ namespace Profiler
         public ExternalTracerAdapter(object tracer)
         {
             _tracer = tracer ?? new NullReferenceException(nameof(tracer));
-            var traceMethod = tracer.GetType().GetMethod("Trace", new Type[] { typeof(IScriptExtent), typeof(ScriptBlock), typeof(int), typeof(string) });
-            _traceMethod = traceMethod ?? throw new InvalidOperationException("The provided tracer does not have Trace method with this signature: Trace(IScriptExtent extent, ScriptBlock scriptBlock, int level, string functionName)");
+            var traceMethod = tracer.GetType().GetMethod("Trace", new Type[] { typeof(IScriptExtent), typeof(ScriptBlock), typeof(int), typeof(string), typeof(string) });
+            _traceMethod = traceMethod ?? throw new InvalidOperationException("The provided tracer does not have Trace method with this signature: Trace(IScriptExtent extent, ScriptBlock scriptBlock, int level, string functionName, string moduleName)");
         }
 
         public void Trace(IScriptExtent extent, ScriptBlock scriptBlock, int level, string functionName, string moduleName)
         {
-            _traceMethod.Invoke(_tracer, new object[] { extent, scriptBlock, level });
+            _traceMethod.Invoke(_tracer, new object[] { extent, scriptBlock, level, functionName, moduleName });
         }
     }
 }
